d0d4a5b5f5e8176b51a19bf71be11705bb58910a
Theodoros Karropoulos <tkarropoulos@gmail.com>
2020-09-03 23:59:46 +0300

    Add more on string extensions

--- src/StringExtensions.cs
+++ src/StringExtensions.cs
@@ -1,15 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Kappa.Extensions
 {
     public static class StringExtensions
     {
-        public static bool IsNullOrEmpty(this string str) => string.IsNullOrEmpty(str);
-        public static bool IsNullOrWhiteSpace(this string str) => string.IsNullOrWhiteSpace(str);
+        public static bool IsNullOrEmpty(this string source) => string.IsNullOrEmpty(source);
+        public static bool IsNullOrWhiteSpace(this string source) => string.IsNullOrWhiteSpace(source);
 
         public static string GetNumbers(this string source)
             => new string(source.Where(c => char.IsDigit(c)).ToArray());
+
+        public static bool IsAlphanumeric(this string source)
+            => source.All(char.IsLetterOrDigit);
+
+        public static string StripAllWhiteSpace(this string source)
+         => Regex.Replace(source.IsNullOrEmpty() ? string.Empty : source, @"\s+", "");
+
+        public static bool ContainsAny(this string source, IEnumerable<string> strings)
+            => strings?.Any(x => source.Contains(x)) ?? false;
+
+        public static bool NotContainNumbers(this string source) 
+            => !(source.Any(char.IsDigit));
     }
 }
