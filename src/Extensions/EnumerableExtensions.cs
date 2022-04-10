89110286c8c3100ec778621cf6c31fa933941747
Theodore Karropoulos <tkarropoulos@gmail.com>
2022-04-10 15:35:49 +0300

    Add enumerable product extension

--- src/Extensions/EnumerableExtensions.cs
+++ src/Extensions/EnumerableExtensions.cs
@@ -6,7 +6,7 @@ namespace Kappa.Extensions
 {
     public static class EnumerableExtensions
     {
-        public static bool NullOrEmpty<T>(this IEnumerable<T> enumerable)
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T> enumerable)
             => enumerable is null || !enumerable.Any();
 
         public static bool HasMoreThanOne<T>(this IEnumerable<T> enumerable)
@@ -36,5 +36,627 @@ namespace Kappa.Extensions
 
         public static IDictionary<TKey, TValue> ToSafeDictionaryOrDefault<TElement, TKey, TValue>(this IEnumerable<TElement> source, Func<TElement, TKey> key, Func<TElement, TValue> value)
             => source?.DistinctBy(key)?.ToDictionary(key, value) ?? new Dictionary<TKey, TValue>();
+
+        public static bool TryGetFirstOrDefault<TSource>(this IEnumerable<TSource> source, out TSource result)
+        {
+            if (source.IsNullOrEmpty())
+            {
+                result = default;
+                return false;
+            }
+
+            result = source.FirstOrDefault();
+            return result != null;
+        }
+
+        public static bool TryGetFirstOrDefault<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate, out TSource result)
+        {
+            if (source.IsNullOrEmpty())
+            {
+                result = default;
+                return false;
+            }
+
+            result = source.FirstOrDefault(predicate);
+            return result != null;
+        }
+
+        public static bool TryGetFirst<TSource>(this IEnumerable<TSource> source, out TSource result)
+        {
+            if (source.IsNullOrEmpty())
+            {
+                result = default;
+                return false;
+            }
+
+            result = source.First();
+            return result != null;
+        }
+
+        public static bool TryGetFirst<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate, out TSource result)
+        {
+            if (source.IsNullOrEmpty())
+            {
+                result = default;
+                return false;
+            }
+
+            result = source.First(predicate);
+            return result != null;
+        }
+
+        public static decimal Product<TSource>(this IEnumerable<TSource> source, Func<TSource, decimal> selector)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (selector == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (!source.Any())
+            {
+                return default;
+            }
+
+            decimal product = 1;
+
+            foreach (var item in source)
+            {
+                var multiplier = selector(item);
+                if (multiplier == 0)
+                {
+                    return multiplier;
+                }
+
+                product *= multiplier;
+            }
+
+            return product;
+        }
+
+        public static double Product<TSource>(this IEnumerable<TSource> source, Func<TSource, double> selector)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (selector == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (!source.Any())
+            {
+                return default;
+            }
+
+            double product = 1;
+
+            foreach (var item in source)
+            {
+                var multiplier = selector(item);
+                if (multiplier == 0)
+                {
+                    return multiplier;
+                }
+
+                product *= multiplier;
+            }
+
+            return product;
+        }
+
+        public static int Product<TSource>(this IEnumerable<TSource> source, Func<TSource, int> selector)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (selector == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (!source.Any())
+            {
+                return default;
+            }
+
+            int product = 1;
+
+            foreach (var item in source)
+            {
+                var multiplier = selector(item);
+                if (multiplier == 0)
+                {
+                    return multiplier;
+                }
+
+                product *= multiplier;
+            }
+
+            return product;
+        }
+
+        public static float Product<TSource>(this IEnumerable<TSource> source, Func<TSource, float> selector)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (selector == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (!source.Any())
+            {
+                return default;
+            }
+
+            float product = 1;
+
+            foreach (var item in source)
+            {
+                var multiplier = selector(item);
+                if (multiplier == 0)
+                {
+                    return multiplier;
+                }
+
+                product *= multiplier;
+            }
+
+            return product;
+        }
+
+        public static long Product<TSource>(this IEnumerable<TSource> source, Func<TSource, long> selector)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (selector == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (!source.Any())
+            {
+                return default;
+            }
+
+            long product = 1;
+
+            foreach (var item in source)
+            {
+                var multiplier = selector(item);
+                if (multiplier == 0)
+                {
+                    return multiplier;
+                }
+
+                product *= multiplier;
+            }
+
+            return product;
+        }
+
+        public static decimal? Product<TSource>(this IEnumerable<TSource> source, Func<TSource, decimal?> selector)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (selector == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (!source.Any())
+            {
+                return default;
+            }
+
+            decimal product = 1;
+
+            foreach (var item in source)
+            {
+                decimal? multiplier = selector(item);
+                if (!multiplier.HasValue || multiplier == 0)
+                {
+                    return 0;
+                }
+
+                product *= multiplier.Value;
+            }
+
+            return product;
+        }
+
+        public static double? Product<TSource>(this IEnumerable<TSource> source, Func<TSource, double?> selector)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (selector == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (!source.Any())
+            {
+                return default;
+            }
+
+            double product = 1;
+
+            foreach (var item in source)
+            {
+                double? multiplier = selector(item);
+                if (!multiplier.HasValue || multiplier == 0)
+                {
+                    return 0;
+                }
+
+                product *= multiplier.Value;
+            }
+
+            return product;
+        }
+
+        public static int? Product<TSource>(this IEnumerable<TSource> source, Func<TSource, int?> selector)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (selector == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (!source.Any())
+            {
+                return default;
+            }
+
+            int product = 1;
+
+            foreach (var item in source)
+            {
+                int? multiplier = selector(item);
+                if (!multiplier.HasValue || multiplier == 0)
+                {
+                    return 0;
+                }
+
+                product *= multiplier.Value;
+            }
+
+            return product;
+        }
+
+        public static float? Product<TSource>(this IEnumerable<TSource> source, Func<TSource, float?> selector)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (selector == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (!source.Any())
+            {
+                return default;
+            }
+
+            float product = 1;
+
+            foreach (var item in source)
+            {
+                float? multiplier = selector(item);
+                if (!multiplier.HasValue || multiplier == 0)
+                {
+                    return 0;
+                }
+
+                product *= multiplier.Value;
+            }
+
+            return product;
+        }
+
+        public static long? Product<TSource>(this IEnumerable<TSource> source, Func<TSource, long?> selector)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (selector == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (!source.Any())
+            {
+                return default;
+            }
+
+            long product = 1;
+
+            foreach (var item in source)
+            {
+                long? multiplier = selector(item);
+                if (!multiplier.HasValue || multiplier == 0)
+                {
+                    return 0;
+                }
+
+                product *= multiplier.Value;
+            }
+
+            return product;
+        }
+
+        public static decimal Product(this IEnumerable<decimal> source)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (!source.Any())
+            {
+                return default;
+            }
+
+            decimal product = 1;
+
+            foreach (var item in source)
+            {
+                product *= item;
+            }
+
+            return product;
+        }
+
+        public static double Product(this IEnumerable<double> source)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (!source.Any())
+            {
+                return default;
+            }
+
+            double product = 1;
+            foreach (var item in source)
+            {
+                product *= item;
+            }
+
+            return product;
+        }
+
+        public static int Product(this IEnumerable<int> source)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (!source.Any())
+            {
+                return default;
+            }
+
+            int product = 1;
+
+            foreach (var item in source)
+            {
+                product *= item;
+            }
+
+            return product;
+        }
+
+        public static float Product(this IEnumerable<float> source)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (!source.Any())
+            {
+                return default;
+            }
+
+            float product = 1;
+
+            foreach (var item in source)
+            {
+                product *= item;
+            }
+
+            return product;
+        }
+
+        public static long Product(this IEnumerable<long> source)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (!source.Any())
+            {
+                return default;
+            }
+
+            long product = 1;
+
+            foreach (var item in source)
+            {
+                product *= item;
+            }
+
+            return product;
+        }
+
+        public static decimal? Product(this IEnumerable<decimal?> source)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (!source.Any())
+            {
+                return default;
+            }
+
+            decimal product = 1;
+
+            foreach (var item in source)
+            {
+                if (!item.HasValue || item == 0)
+                {
+                    return 0;
+                }
+
+                product *= item.Value;
+            }
+
+            return product;
+        }
+
+        public static double? Product(this IEnumerable<double?> source)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (!source.Any())
+            {
+                return default;
+            }
+
+            double product = 1;
+            
+            foreach (var item in source)
+            {
+                if (!item.HasValue || item == 0)
+                {
+                    return 0;
+                }
+
+                product *= item.Value;
+            }
+
+            return product;
+        }
+
+        public static int? Product(this IEnumerable<int?> source)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (!source.Any())
+            {
+                return default;
+            }
+
+            int product = 1;
+
+            foreach (var item in source)
+            {
+                if (!item.HasValue || item == 0)
+                {
+                    return 0;
+                }
+
+                product *= item.Value;
+            }
+
+            return product;
+        }
+
+        public static float? Product(this IEnumerable<float?> source)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (!source.Any())
+            {
+                return default;
+            }
+
+            float product = 1;
+
+            foreach (var item in source)
+            {
+                if (!item.HasValue || item == 0)
+                {
+                    return 0;
+                }
+
+                product *= item.Value;
+            }
+
+            return product;
+        }
+
+        public static long? Product(this IEnumerable<long?> source)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (!source.Any())
+            {
+                return default;
+            }
+
+            long product = 1;
+
+            foreach (var item in source)
+            {
+                if (!item.HasValue || item == 0)
+                {
+                    return 0;
+                }
+
+                product *= item.Value;
+            }
+
+            return product;
+        }
     }
 }
--- tests/Kappa.Extensions.UnitTests/EnumerableExtensionsTests.cs
+++ tests/Kappa.Extensions.UnitTests/EnumerableExtensionsTests.cs
@@ -2,17 +2,25 @@ using System.Collections.Generic;
 using Xunit;
 using FluentAssertions;
 using System.Linq;
+using AutoFixture;
 
 namespace Kappa.Extensions.UnitTests
 {
     public class EnumerableExtensionsTests
     {
+        private readonly Fixture _fixture;
+
+        public EnumerableExtensionsTests()
+        {
+            _fixture = new Fixture();
+        }
+
         [Fact]
         public void NullOrEmpty_Should_Return_False()
         {
             var enumerable = new List<string> { "test", "demo" };
 
-            enumerable.NullOrEmpty().Should().BeFalse();
+            enumerable.IsNullOrEmpty().Should().BeFalse();
         }
 
         [Fact]
@@ -20,7 +28,7 @@ namespace Kappa.Extensions.UnitTests
         {
             var enumerable = new List<string>();
 
-            enumerable.NullOrEmpty().Should().BeTrue();
+            enumerable.IsNullOrEmpty().Should().BeTrue();
         }
 
         [Fact]
@@ -28,7 +36,7 @@ namespace Kappa.Extensions.UnitTests
         {
             IEnumerable<string> enumerable = null;
 
-            enumerable.NullOrEmpty().Should().BeTrue();
+            enumerable.IsNullOrEmpty().Should().BeTrue();
         }
 
         [Fact]
@@ -49,7 +57,7 @@ namespace Kappa.Extensions.UnitTests
             var safeDict = dict.ToSafeDictionaryOrDefault(x => x, y => y);
             var douplicates = safeDict.GroupBy(x => x).Where(x => x.Count() > 1).ToList();
 
-            Assert.True(douplicates.NullOrEmpty());
+            Assert.True(douplicates.IsNullOrEmpty());
         }
 
         [Fact]
@@ -57,12 +65,407 @@ namespace Kappa.Extensions.UnitTests
         {
             var dict = new List<object>
             {
-                
+
             };
 
             var safeDict = dict.ToSafeDictionaryOrDefault(x => x, y => y);
 
-            Assert.True(safeDict.NullOrEmpty());
+            Assert.True(safeDict.IsNullOrEmpty());
         }
+
+        [Fact]
+        public void TryGetFirstOrDefault_Should_Return_True()
+        {
+            var source = new List<int> { 1, 2, 3, 4, 5 };
+            var result = source.TryGetFirstOrDefault(out var value);
+
+            Assert.True(result);
+            Assert.True(value == source.FirstOrDefault());
+        }
+
+        [Fact]
+        public void TryGetFirstOrDefault_With_Predicate_Should_Return_True()
+        {
+            var source = new List<int> { 1, 2, 3, 4, 5 };
+            var result = source.TryGetFirstOrDefault(x => x == 2, out var value);
+
+            Assert.True(result);
+            Assert.True(value == 2);
+        }
+
+        [Fact]
+        public void TryGetFirstOrDefault_Should_Return_False()
+        {
+            var source = new List<int> { };
+            var result = source.TryGetFirstOrDefault(out var value);
+
+            Assert.False(result);
+            Assert.True(value == default);
+        }
+
+        [Fact]
+        public void TryGetFirstOrDefault_With_Predicate_Should_Return_False()
+        {
+            var source = new List<Student>();
+            var result = source.TryGetFirstOrDefault(x => x.Id == 20, out var value);
+
+            Assert.False(result);
+            Assert.True(value == default);
+        }
+
+        [Fact]
+        public void Will_calculate_product_of_a_decimal_list()
+        {
+            var list = _fixture.CreateMany<decimal>(5);
+
+            var product = list.Product();
+
+            var result = 1m;
+            foreach (var item in list)
+            {
+                result *= item;
+            }
+
+            Assert.Equal(result, product);
+        }
+
+        [Fact]
+        public void Will_calculate_product_of_a_double_list()
+        {
+            var list = _fixture.CreateMany<double>(5);
+
+            var product = list.Product();
+
+            var result = 1d;
+            foreach(var item in list)
+            {
+                result *= item;
+            }
+
+            Assert.Equal(result, product);
+        }
+
+        [Fact]
+        public void Will_calculate_product_of_a_float_list()
+        {
+            var list = _fixture.CreateMany<float>(5);
+
+            var product = list.Product();
+
+            var result = 1f;
+            foreach (var item in list)
+            {
+                result *= item;
+            }
+
+            Assert.Equal(result, product);
+        }
+
+        [Fact]
+        public void Will_calculate_product_of_an_int_list()
+        {
+            var list = _fixture.CreateMany<int>(5);
+
+            var product = list.Product();
+
+            var result = 1;
+            foreach (var item in list)
+            {
+                result *= item;
+            }
+
+            Assert.Equal(result, product);
+        }
+
+        [Fact]
+        public void Will_calculate_product_of_a_long_list()
+        {
+            var list = _fixture.CreateMany<long>(5);
+
+            var product = list.Product();
+
+            var result = 1L;
+            foreach (var item in list)
+            {
+                result *= item;
+            }
+
+            Assert.Equal(result, product);
+        }
+
+        [Fact]
+        public void Will_calculate_zero_product_of_a_decimal_list()
+        {
+            var list = _fixture.CreateMany<decimal>(5).ToList();
+            list.Add(0);
+
+            var product = list.Product();
+
+            Assert.Equal(0, product);
+        }
+
+        [Fact]
+        public void Will_calculate_zero_product_of_a_double_list()
+        {
+            var list = _fixture.CreateMany<double>(5).ToList();
+            list.Add(0);
+
+            var product = list.Product();
+
+            Assert.Equal(0, product);
+        }
+
+        [Fact]
+        public void Will_calculate_zero_product_of_a_float_list()
+        {
+            var list = _fixture.CreateMany<float>(5).ToList();
+            list.Add(0);
+
+            var product = list.Product();
+
+            Assert.Equal(0, product);
+        }
+
+        [Fact]
+        public void Will_calculate_zero_product_of_an_int_list()
+        {
+            var list = _fixture.CreateMany<int>(5).ToList();
+            list.Add(0);
+
+            var product = list.Product();
+
+            Assert.Equal(0, product);
+        }
+
+        [Fact]
+        public void Will_calculate_zero_product_of_a_long_list()
+        {
+            var list = _fixture.CreateMany<long>(5).ToList();
+            list.Add(0);
+
+            var product = list.Product();
+
+            Assert.Equal(0, product);
+        }
+
+        [Fact]
+        public void Will_calculate_zero_product_of_an_empty_decimal_list()
+        {
+            var list = new List<decimal>();
+
+            var product = list.Product();
+
+            Assert.Equal(0, product);
+        }
+
+        [Fact]
+        public void Will_calculate_zero_product_of_an_empty_double_list()
+        {
+            var list = new List<double>();
+
+            var product = list.Product();
+
+            Assert.Equal(0, product);
+        }
+
+        [Fact]
+        public void Will_calculate_zero_product_of_an_empty_float_list()
+        {
+            var list = new List<float>();
+
+            var product = list.Product();
+
+            Assert.Equal(0, product);
+        }
+
+        [Fact]
+        public void Will_calculate_zero_product_of_an_empty_int_list()
+        {
+            var list = new List<long>();
+
+            var product = list.Product();
+
+            Assert.Equal(0, product);
+        }
+
+        [Fact]
+        public void Will_calculate_zero_product_of_an_empty_long_list()
+        {
+            var list = new List<long>();
+
+            var product = list.Product();
+
+            Assert.Equal(0, product);
+        }
+
+        [Fact]
+        public void Will_calculate_product_of_a_nullable_decimal_list()
+        {
+            var list = _fixture
+                .Build<decimal?>()
+                .CreateMany(5);
+
+            decimal? result = 1;
+            foreach(var item in list)
+            {
+                result *= item;
+            }
+
+            var product = list.Product();
+
+            Assert.Equal(product, result);
+        }
+
+        [Fact]
+        public void Will_calculate_product_of_a_nullable_double_list()
+        {
+            var list = _fixture
+                .Build<double?>()
+                .CreateMany(5);
+
+            double? result = 1;
+            foreach (var item in list)
+            {
+                result *= item;
+            }
+
+            var product = list.Product();
+
+            Assert.Equal(product, result);
+        }
+
+        [Fact]
+        public void Will_calculate_product_of_a_nullable_float_list()
+        {
+            var list = _fixture
+                .Build<float?>()
+                .CreateMany(5);
+
+            float? result = 1;
+            foreach (var item in list)
+            {
+                result *= item;
+            }
+
+            var product = list.Product();
+
+            Assert.Equal(product, result);
+        }
+
+        [Fact]
+        public void Will_calculate_product_of_a_nullable_integer_list()
+        {
+            var list = _fixture
+                .Build<int?>()
+                .CreateMany(5);
+
+            int? result = 1;
+            foreach (var item in list)
+            {
+                result *= item;
+            }
+
+            var product = list.Product();
+
+            Assert.Equal(product, result);
+        }
+
+        [Fact]
+        public void Will_calculate_product_of_a_nullable_long_list()
+        {
+            var list = _fixture
+                .Build<long?>()
+                .CreateMany(5);
+
+            long? result = 1;
+            foreach (var item in list)
+            {
+                result *= item;
+            }
+
+            var product = list.Product();
+
+            Assert.Equal(product, result);
+        }
+
+        [Fact]
+        public void Will_calculate_zero_product_of_a_nullable_decimal_list()
+        {
+            var list = _fixture
+                .Build<decimal?>()
+                .CreateMany(5)
+                .ToList();
+
+            list.Add(null);
+
+            var product = list.Product();
+
+            Assert.Equal(0, product);
+        }
+
+        [Fact]
+        public void Will_calculate_zero_product_of_a_nullable_double_list()
+        {
+            var list = _fixture
+                .Build<double?>()
+                .CreateMany(5)
+                .ToList();
+
+            list.Add(null);
+
+            var product = list.Product();
+
+            Assert.Equal(0, product);
+        }
+
+        [Fact]
+        public void Will_calculate_zero_product_of_a_nullable_float_list()
+        {
+            var list = _fixture
+                .Build<float?>()
+                .CreateMany(5)
+                .ToList();
+
+            list.Add(null);
+
+            var product = list.Product();
+
+            Assert.Equal(0, product);
+        }
+
+        [Fact]
+        public void Will_calculate_zero_product_of_a_nullable_integer_list()
+        {
+            var list = _fixture
+                .Build<int?>()
+                .CreateMany(5)
+                .ToList();
+
+            list.Add(null);
+
+            var product = list.Product();
+
+            Assert.Equal(0, product);
+        }
+
+        [Fact]
+        public void Will_calculate_zero_product_of_a_nullable_long_list()
+        {
+            var list = _fixture
+                .Build<long?>()
+                .CreateMany(5)
+                .ToList();
+
+            list.Add(null);
+
+            var product = list.Product();
+
+            Assert.Equal(0, product);
+        }
+    }
+
+    class Student
+    {
+        public int Id { get; set; }
     }
 }
